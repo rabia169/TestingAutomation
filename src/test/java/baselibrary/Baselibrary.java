3a5eae8d82d7cbdaafb618332b8b9700cb503837
rabia169 <rabiaperween808@gmail.com>
2026-05-26 18:27:47 +0530

    my first commit

---
+++ src/test/java/baselibrary/Baselibrary.java
@@ -0,0 +1,16 @@
+package baselibrary;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Baselibrary 
+{
+	@Test
+	public void launchURL()
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.get("https:testingbaba.com/old/");
+	}
+
+}
